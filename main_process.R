cf79a005ae8704818e20b6e60319aa8312f6792e
awp-finanznachrichten <sw@awp.ch>
2023-10-22 10:18:42 +0200

    commit from Rstudio

--- main_process.R
+++ main_process.R
@@ -1,5 +1,5 @@
 #Working Directory
-MAIN_PATH <- "C:/Users/simon/OneDrive/"
+MAIN_PATH <- "C:/Users/sw/OneDrive/"
 setwd(paste0(MAIN_PATH,"sda_eidgenoessische_wahlen_datafeed"))
 
 #Get Libraries and needed Data
